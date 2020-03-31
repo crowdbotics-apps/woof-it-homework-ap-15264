7ea3ee01cd5ca3062b0e84d851b410125dd9b5e9
Crowdbotics <team@crowdbotics.com>
2020-03-31 15:06:55 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/woof_it_homework_ap_15264/MainActivity.java
@@ -0,0 +1,15 @@
+package com.woof_it_homework_ap_15264;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "woof_it_homework_ap_15264";
+    }
+}
